fb7f8fbbb2c9be58ed8c90b19f9ba6a8a5907409
rmd371 <44234254+rmd371@users.noreply.github.com>
2020-10-03 13:36:07 -0500

    Update ReaderTests.purs

--- ReaderTests.purs
+++ ReaderTests.purs
@@ -11,41 +11,36 @@ import Control.Monad.Reader.Class (class MonadAsk, ask)
 
 newtype Reader e a = Reader (e -> a)
 
-runReader :: forall e a. Reader e a -> e -> a
-runReader (Reader renderFn) env = renderFn env 
+-- TODO: comment in the unit tests below after you've implemented the associated method
+
+-- runReader :: forall e a. Reader e a -> e -> a
 
 --derive newtype instance semigroupOp :: Semigroup a ⇒ Semigroup (Reader s a)
-instance semigroupReader :: Semigroup a => Semigroup (Reader e a) where
-  -- append :: a -> a -> a
-  -- append :: Reader e a -> Reader e a -> Reader e a
-  append (Reader render1) (Reader render2) = Reader \env -> render1 env <> render2 env
+-- instance semigroupReader :: Semigroup a => Semigroup (Reader e a) where
+--   -- append :: a -> a -> a
+--   -- append :: Reader e a -> Reader e a -> Reader e a
 
 -- derive newtype instance functorStat3 :: Functor (Reader s)
-instance functorReader :: Functor (Reader s) where
-  -- map :: forall a b. (a -> b) -> f a -> f b
-  map mapFn (Reader render) = Reader \env -> mapFn $ render env
+-- instance functorReader :: Functor (Reader s) where
+--   -- map :: forall a b. (a -> b) -> f a -> f b
 
 -- derive newtype instance applyReader :: Apply (Reader e)
-instance applyReader :: Apply (Reader e) where
-  -- apply :: forall a b. f (a -> b) -> f a -> f b
-  apply (Reader f) (Reader r) = Reader \e -> f e $ r e
+-- instance applyReader :: Apply (Reader e) where
+--   -- apply :: forall a b. f (a -> b) -> f a -> f b
 
 --derive newtype instance applicativeReader :: Apply (Reader e) => Applicative (Reader e)
-instance applicativeReader :: Applicative (Reader s) where
-  -- pure :: forall a. a -> f a
-  pure e = Reader (\_ -> e)
+-- instance applicativeReader :: Applicative (Reader s) where
+--   -- pure :: forall a. a -> f a
 
 -- derive newtype instance bindReader :: Bind (Reader e)
-instance bindReader :: Bind (Reader e) where
-  -- bind :: forall a b. m a -> (a -> m b) -> m b
-  bind (Reader render) otherReaderFn = Reader \env -> runReader (otherReaderFn $ render env) env
+-- instance bindReader :: Bind (Reader e) where
+--   -- bind :: forall a b. m a -> (a -> m b) -> m b
 
-instance monadReader :: Monad (Reader e)
+-- instance monadReader :: Monad (Reader e)
 
 -- -- derive newtype instance monadAskReader :: MonadAsk e (Reader e)
-instance askReader :: MonadAsk e (Reader e) where
-  -- ask :: forall e a. (e -> a) -> Reader e a
-  ask = Reader (\e -> e)
+-- instance askReader :: MonadAsk e (Reader e) where
+--   -- ask :: forall e a. (e -> a) -> Reader e a
 
 type Env = { type :: String, buildType :: String, symbol :: String, rank :: Int }
 
@@ -70,7 +65,6 @@ rankToStr = \x -> "#" <> show x
 rankToPhraseRdr :: Reader Env (Int -> String)
 rankToPhraseRdr = Reader $ \e -> \i -> e.buildType <> "ing for " <> rankToStr i
 
--- comment in tests after you've implemented the associated method above
 main :: Effect Unit
 main = do 
   render $ h1 $ text "Build my own Reader tests"
@@ -79,15 +73,15 @@ main = do
       suite "Reader" do
         test "runReader" do
           Assert.assert "returns 'pure'" $ runReader typeRdr env == "pure"
-        test "append" do
-          Assert.assert "returns 'purescript'" $ runReader typePlusBuildType env == "purescript"
-        test "map" do
-          Assert.assert "return '#1'" $ runReader (map rankToStr rankRdr) env == "#1"
-        test "apply" do
-          Assert.assert "return 'scripting for #1'" $ runReader (apply rankToPhraseRdr rankRdr) env == "scripting for #1"
-        test "pure/of" do
-          Assert.assert "return 'is'" $ runReader (pure "is") env == "is"
-        test "bind/chain" do
-          Assert.assert "return '#1'" $ runReader (bind typePlusBuildType (\x -> pure $ x <> " is fun")) env == "purescript is fun"
-        test "ask" do
-          Assert.assert "return '#'" $ runReader ask (\e -> e.symbol) env == "#"
+        -- test "append" do
+        --   Assert.assert "returns 'purescript'" $ runReader typePlusBuildType env == "purescript"
+        -- test "map" do
+        --   Assert.assert "return '#1'" $ runReader (map rankToStr rankRdr) env == "#1"
+        -- test "apply" do
+        --   Assert.assert "return 'scripting for #1'" $ runReader (apply rankToPhraseRdr rankRdr) env == "scripting for #1"
+        -- test "pure/of" do
+        --   Assert.assert "return 'is'" $ runReader (pure "is") env == "is"
+        -- test "bind/chain" do
+        --   Assert.assert "return '#1'" $ runReader (bind typePlusBuildType (\x -> pure $ x <> " is fun")) env == "purescript is fun"
+        -- test "ask" do
+        --   Assert.assert "return '#'" $ runReader ask (\e -> e.symbol) env == "#"
